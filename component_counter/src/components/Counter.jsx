443e8f20dffe98ab4c01d74adfe3f1b0e015f8ea
ngocsinhnguyen <nguyenns6802@gmail.com>
2026-02-04 08:43:33 +0700

    s4

--- component_counter/src/components/Counter.jsx
+++ component_counter/src/components/Counter.jsx
@@ -1,13 +1,11 @@
 import { useState} from "react";
 
 function Counter() {
-    // Khởi tạo state count với giá trị ban đầu là 0
     const [count, setCount] = useState(0);
 
-    // Hàm xử lý sự kiện khi click nút
     const handleClick = () => {
         const newValue = count + 1;
-        setCount(newValue); // Cập nhật state => giao diện tự render lại
+        setCount(newValue);
     };
 
     return (
